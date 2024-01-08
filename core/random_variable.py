decd1a52fb1e3c04ea7b71ad53691ad4766b00f2
Botzer <abotzer@mba2024.hbs.edu>
2024-01-08 10:09:12 +0200

    random variables

--- core/random_variable.py
+++ core/random_variable.py
@@ -1,53 +1,87 @@
 """Random variables implelementations."""
 
+from abc import abstractmethod, ABC
 
-from typing import Dict
+import numpy as np
+from scipy import stats
 
 
-class RandomVariable:
+class RandomVariable(ABC):
     """Random variable class."""
-    def __init__(self) -> None:
-        pass
+    @abstractmethod
+    def sample(self):
+        """Sample from the random variable."""
 
 
-class DiscreteRandomVariable(RandomVariable):
-    """Discrete random variable class."""
-    def __init__(self, probabilities_dict: Dict) -> None:
-        self.probabilities_dict = probabilities_dict
-        self.values = list(probabilities_dict.keys())
-        self.probabilities = list(probabilities_dict.values())
-        assert sum(self.probabilities) == 1, "Probabilities must sum to 1."
+class BinomialRandomVariable(RandomVariable):
+    """Binomial random variable class."""
+    def __init__(self, n, p) -> None:
+        self.n = n
+        self.p = p
+        self.variable = np.random.binomial
+    
+    def sample(self):
+        return self.variable(self.n, self.p)
 
-    def get_probability(self, value):
-        """Returns the probability of a value."""
-        return self.probabilities_dict[value]
+
+class PoissonRandomVariable(RandomVariable):
+    """Poisson random variable class"""
+    def __init__(self, lmbda) -> None:
+        self.lmbda = lmbda
+        self.variable = stats.poisson
+
+    def sample(self):
+        return self.variable(self.lmbda)
+
+
+class GeometricRandomVariable(RandomVariable):
+    """Geometric random variable class."""
+    def __init__(self, p) -> None:
+        super().__init__()
+        self.p = p
+        self.variable = stats.geom
+
+
+class NegativeBinomialRandomVariable(RandomVariable):
+    """Negative binomial random variable""" 
+    def __init__(self, r, p) -> None:
+        super().__init__()
+        self.r = r
+        self.p = p
+        self.variable = stats.nbinom
+
+
+class UniformRandomVariable(RandomVariable):
+    """Continuous uniform random variable"""
+    def __init__(self, a, b) -> None:
+        super().__init__()
+        self.a = a
+        self.b = b
+        self.variable = np.random.uniform
+
+
+class ExponentialRandomVariable(RandomVariable):
+    """Exponential random variable class."""
+    def __init__(self, lmbda) -> None:
+        super().__init__()
+        self.lmbda = lmbda
+        self.variable = stats.expon
+
+
+class GammaRandomVariable(RandomVariable):
+    """Gamma random variable class."""
+    def __init__(self, alpha, beta) -> None:
+        super().__init__()
+        self.alpha = alpha
+        self.beta = beta
+        self.variable = stats.gamma
 
 
 class NormalRandomVariable(RandomVariable):
     """Normal random variable class."""
-    def __init__(self, mean: float, std: float) -> None:
-        self.mean = mean
-        self.std = std
+    def __init__(self, mu, sigma) -> None:
+        super().__init__()
+        self.mu = mu
+        self.sigma = sigma
+        self.variable = stats.norm
 
-    def get_mean(self):
-        """Returns the mean."""
-        return self.mean
-
-    def get_std(self):
-        """Returns the standard deviation."""
-        return self.std
-
-
-class UniformRandomVariable(RandomVariable):
-    """Uniform random variable class."""
-    def __init__(self, lower_bound: float, upper_bound: float) -> None:
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-
-    def get_lower_bound(self):
-        """Returns the lower bound."""
-        return self.lower_bound
-
-    def get_upper_bound(self):
-        """Returns the upper bound."""
-        return self.upper_bound
